781485c1c7ca04c735d9c542f2c1353ac900d049
samsegomof <wemagonof91@gmail.com>
2023-03-10 02:21:38 +0500

    fix some error

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
-import os
+# import os
 
-from dotenv import load_dotenv
+# from dotenv import load_dotenv
 
-load_dotenv()   # Функция для загрузки переменных из .env
+# load_dotenv()   # Функция для загрузки переменных из .env
 
-BOT_TOKEN = str(os.getenv('BOT_TOKEN'))
-MASTER_CHAT_ID = os.getenv('MASTER_CHAT_ID')  # chat_id мастера
+BOT_TOKEN = '6233131407:AAHaie1JR4PoTS2b8B7H-dFsC11DQj2F4eo'  # str(os.getenv('BOT_TOKEN'))
+MASTER_CHAT_ID = 744258788  # os.getenv('MASTER_CHAT_ID')  # chat_id мастера
