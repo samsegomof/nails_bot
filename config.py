6a1aeae5e6a043cb07936c43fe15ba27d58f2e24
samsegomof <wemagonof91@gmail.com>
2023-03-10 02:18:08 +0500

    revoke token

--- config.py
+++ config.py
@@ -4,5 +4,5 @@ from dotenv import load_dotenv
 
 load_dotenv()   # Функция для загрузки переменных из .env
 
-BOT_TOKEN = str(os.getenv('BOT_TOKEN'))  # Телеграм API - токен
+BOT_TOKEN = str(os.getenv('BOT_TOKEN'))
 MASTER_CHAT_ID = os.getenv('MASTER_CHAT_ID')  # chat_id мастера
