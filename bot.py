b01275f1c64da8d2be08902f43d085a4260d9d03
samsegomof <wemagonof91@gmail.com>
2023-03-12 12:33:25 +0500

    upd 12.03 - change bot structure and hadlers logic

--- bot.py
+++ bot.py
@@ -11,7 +11,8 @@ from aiogram.dispatcher.filters.state import State, StatesGroup
 from config import BOT_TOKEN, MASTER_CHAT_ID
 from accessors.db_accessor import Database
 from markups import start_message, date_message, wrong_date_message, \
-    need_correct_date_message, time_message, uncorrect_time_message, phone_message
+    need_correct_date_message, time_message, uncorrect_time_message, phone_message, url_buttons
+
 
 """Файл инициализации и запуска бота"""
 
@@ -29,98 +30,118 @@ class StateMachine(StatesGroup):
     phone_waiting = State()
 
 
-async def send_notification_to_master(name: str, date: datetime, time: datetime, phone: str):
-    """Функция отправки уведомления мастеру о новой записи"""
-    message = (
-        f'Новая запись!\n'
-        f'Имя: {name}\n'
-        f'Дата: {date.strftime("%d.%m.%Y")}\n'
-        f'Время: {time.strftime("%H:%M")}\n'
-        f'Телефон: {phone}'
-    )
-    await bot.send_message(chat_id=MASTER_CHAT_ID, text=message)
+# async def send_notification_to_master(name: str, date: datetime, time: datetime, phone: str):
+#     """Функция отправки уведомления мастеру о новой записи"""
+#     message = (
+#         f'Новая запись!\n'
+#         f'Имя: {name}\n'
+#         f'Дата: {date.strftime("%d.%m.%Y")}\n'
+#         f'Время: {time.strftime("%H:%M")}\n'
+#         f'Телефон: {phone}'
+#     )
+#     await bot.send_message(chat_id=MASTER_CHAT_ID, text=message)
 
 
 @dp.message_handler(commands=['start'])
 async def cmd_start(message: types.Message):
-    """Обработчик команды старт"""
-    await message.answer(start_message)
-    await StateMachine.name_waiting.set()
-
-
-@dp.message_handler(state=StateMachine.name_waiting)
-async def get_name(message: types.Message, state: FSMContext):
-    """Обработчик отправки имени"""
-    name = message.text
-    await state.update_data(name=name)
-
-    await message.answer(date_message)
-    await StateMachine.date_waiting.set()
-
-
-@dp.message_handler(state=StateMachine.date_waiting)
-async def get_date(message: types.Message, state: FSMContext):
     """
-    Получение даты от пользователя, если формат неверный -
-    шлет исключение и просит ввести корректный формат
+    Обработчик команды старт
     """
-    try:
-        date = datetime.strptime(message.text, '%d.%m.%Y')
-    except ValueError:
-        await message.answer(wrong_date_message)
-    else:
-        now = datetime.now()
-        if date < now:
-            await message.answer(need_correct_date_message)
-            return
-
-    await state.update_data(date=date)
-    await message.answer(time_message)
-    await StateMachine.time_waiting.set()
+    await message.answer(start_message, reply_markup=url_buttons)
 
 
-@dp.message_handler(state=StateMachine.time_waiting)
-async def get_time(message: types.Message, state: FSMContext):
+@dp.message_handler(commands=['cancel'], state='*')
+async def cancel_handler(message: types.Message, state: FSMContext):
     """
-    Получение времени от пользователя, если формат неверный -
-    шлет исключение и просит ввести верный формат
+    Обработчик команды отмены
     """
-    try:
-        time = datetime.strptime(message.text, '%H:%M').time()
-    except ValueError:
-        await message.answer(uncorrect_time_message)
-
-    await state.update_data(time=time)
-    await message.answer(phone_message)
-    await StateMachine.phone_waiting.set()
-    return
+    await state.finish()
+    await message.answer('Вы отменили свою запись')
 
 
-@dp.message_handler(state=StateMachine.phone_waiting)
-async def get_phone(message: types.Message, state: FSMContext):
-    """
-    Получение номера телефона, запись данных в бд и отправка уведомления мастеру о новой записи
-    """
-    phone = message.text
-    await state.update_data(phone=phone)
+# @dp.callback_query_handler(text='Записаться')
+# async def reserve_handler(callback: types.CallbackQuery):
+#     """
+#     Обработчик кнопки 'Записаться'
+#     """
+#     await callback.message.answer(text='Введите ваше имя:\n')
+#     await callback.answer()
+#     await StateMachine.name_waiting.set()
 
-    data = await state.get_data()
-    name = data.get('name')
-    date = data.get('date')
-    time = data.get('time')
 
-    await message.answer(
-        f'Вы записались на '
-        f'{date.strftime("%d.%m.%Y")}\n'
-        f'в {time.strftime("%H:%M")}.\n'
-        f'Ваше имя: {name}\n'
-        f'Ваш номер телефона: {phone}\n'
-        f'Я с вами свяжусь по указанному номеру телефона для подтверждения записи!\n'
-        f'Если у вам нужно отменить посещение, пожалуйста позвоните по номеру +79875960877'
-    )
-    with Database('appointments.db') as db:
-        db.add_reservation(name, date.strftime('%Y-%m-%d'), time.strftime('%H:%M'), phone)
-    await send_notification_to_master(name, date, time, phone)
+# @dp.message_handler(state=StateMachine.name_waiting)
+# async def get_name(message: types.Message, state: FSMContext):
+#     """Обработчик отправки имени"""
+#     name = message.text
+#     await state.update_data(name=name)
+#
+#     await message.answer(date_message)
+#     await StateMachine.date_waiting.set()
+
+
+# @dp.message_handler(state=StateMachine.date_waiting)
+# async def get_date(message: types.Message, state: FSMContext):
+#     """
+#     Получение даты от пользователя, если формат неверный -
+#     шлет исключение и просит ввести корректный формат
+#     """
+#     try:
+#         date = datetime.strptime(message.text, '%d.%m.%Y')
+#     except ValueError:
+#         await message.answer(wrong_date_message)
+#     else:
+#         now = datetime.now()
+#         if date < now:
+#             await message.answer(need_correct_date_message)
+#             return
+
+    # await state.update_data(date=date)
+    # await message.answer(time_message)
+    # await StateMachine.time_waiting.set()
+
+
+# @dp.message_handler(state=StateMachine.time_waiting)
+# async def get_time(message: types.Message, state: FSMContext):
+#     """
+#     Получение времени от пользователя, если формат неверный -
+#     шлет исключение и просит ввести верный формат
+#     """
+#     try:
+#         time = datetime.strptime(message.text, '%H:%M').time()
+#     except ValueError:
+#         await message.answer(uncorrect_time_message)
+#
+#     await state.update_data(time=time)
+#     await message.answer(phone_message)
+#     await StateMachine.phone_waiting.set()
+#     return
+
+
+# @dp.message_handler(state=StateMachine.phone_waiting)
+# async def get_phone(message: types.Message, state: FSMContext):
+#     """
+#     Получение номера телефона, запись данных в бд и отправка уведомления мастеру о новой записи
+#     """
+#     phone = message.text
+#     await state.update_data(phone=phone)
+#
+#     data = await state.get_data()
+#     name = data.get('name')
+#     date = data.get('date')
+#     time = data.get('time')
+#
+#     await message.answer(
+#         f'Вы записались на '
+#         f'{date.strftime("%d.%m.%Y")}\n'
+#         f'в {time.strftime("%H:%M")}.\n'
+#         f'Ваше имя: {name}\n'
+#         f'Ваш номер телефона: {phone}\n'
+#         f'Я с вами свяжусь по указанному номеру телефона для подтверждения записи!\n'
+#         f'Если у вам нужно отменить посещение, пожалуйста позвоните по номеру +79875960877'
+#     )
+#     with Database('appointments.db') as db:
+#         db.add_reservation(name, date.strftime('%Y-%m-%d'), time.strftime('%H:%M'), phone)
+#     await send_notification_to_master(name, date, time, phone)
 
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True)
--- markups.py
+++ markups.py
@@ -1,11 +1,37 @@
-"""Переменные текстовых сообщений"""
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
-start_message = 'Привет! Я бот мастера Адель! ' \
-                'Напиши мне свое имя.'
 
+"""Callback buttons"""
+reserve_btn = InlineKeyboardButton('Записаться', callback_data='Записаться')
+cancel_btn = InlineKeyboardButton('Отмена', callback_data='cancel_btn')
+contact_btn = InlineKeyboardButton('Контакты', callback_data='contact_btn')
+price_btn = InlineKeyboardButton('Прайс', callback_data='price_btn')
+schedule_btn = InlineKeyboardButton('Расписание', callback_data='schedule_btn')
+menu_markup = InlineKeyboardMarkup(row_width=2)
+menu_markup.add(reserve_btn, cancel_btn, contact_btn, price_btn, schedule_btn)
+
+"""URL buttons"""
+url_btn_order = InlineKeyboardButton(text='Записаться', url='https://n827632.yclients.com/')
+url_btn_inst = InlineKeyboardButton(
+    text='Наш инстаграм',
+    url='https://instagram.com/akinfeeva_nails?igshid=YmMyMTA2M2Y='
+    )
+url_btn_location = InlineKeyboardButton(text='Где мы находимся', url='https://go.2gis.com/sj3uo')
+url_buttons = InlineKeyboardMarkup(row_width=2)
+url_buttons.add(url_btn_order, url_btn_inst, url_btn_location)
+
+"""Messages"""
+start_message = 'Привет! Я бот салона Akinfeeva Nails!\n' \
+                'Знаем всё про крепкие и красивые ногти!\n' \
+                'ИДЕАЛЬНЫЙ МАНИКЮР ЗА 1.5 часа 🚀\n' \
+                'ПЕРВЫЙ РАЗ? Скидка 200р. на первое посещение\n' \
+                'Запись в один клик❤️\n' \
+                'Выбери, что ты хочешь сделать 💅🏼'
+welcome_message = 'Привет, если вам нужен качественный маникюр или педикюр, то вы пришли по адресу!\n'
 date_message = 'Когда Вы хотите записаться на прием? (в формате ДД.ММ.ГГГГ)'
 wrong_date_message = 'Неверный формат даты. Пожалуйста, введите дату в формате ДД.ММ.ГГГГ'
 need_correct_date_message = 'Введите предстоящую дату, записаться в прошлое, увы невозможно)'
 time_message = 'На какое время вы хотели бы записаться? Напишите в формате ЧЧ:ММ'
 uncorrect_time_message = 'Неверный формат времени. Пожалуйста, введите время в формате ЧЧ:ММ'
 phone_message = 'Напишите свой номер телефона для связи'
+
